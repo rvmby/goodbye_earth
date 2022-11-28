76d028416e245e6c63ef58700b549a74630f7bdc
Natalia Eiflander Braganca <nataliabragancan@gmail.com>
2022-11-28 17:28:57 +0100

    add model controller view Spaceship

---
+++ app/controllers/spaceships_controller.rb
@@ -0,0 +1,13 @@
+class SpaceshipsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ test/controllers/spaceships_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class SpaceshipsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get spaceships_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get spaceships_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get spaceships_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get spaceships_create_url
+    assert_response :success
+  end
+end
