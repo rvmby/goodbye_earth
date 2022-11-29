d0e16d56449fbc1c10bccf266ba4a1c54b84c84a
Perdita Edge-Partington <perdita.edge.partington@gmail.com>
2022-11-29 14:01:51 +0000

    adding pundit

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,13 +1,44 @@
 class SpaceshipsController < ApplicationController
+  before_action :find_spaceship, only: %w[show edit update destroy]
+
   def index
+    @spaceships = policy_scope(Spaceship)
   end
 
   def show
+    authorize @spaceship
   end
 
   def new
+    @spaceship = Spaceship.new
+    authorize @spaceship
   end
 
   def create
+    @spaceship = Spaceship.new(spaceship_params)
+    @spaceship.user = current_user
+    authorize @spaceship
+  end
+
+  def edit
+    authorize @spaceship
+  end
+
+  def update
+    authorize @spaceship
+  end
+
+  def destroy
+    authorize @spaceship
+  end
+
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :description, :max_people, :price)
+  end
+
+  def find_spaceship
+    @spaceship = Spaceship.find(params[:id])
   end
 end
