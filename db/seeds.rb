697e3267009f4b77842ea08690df810ce905050d
Natalia Eiflander Braganca <nataliabragancan@gmail.com>
2022-12-01 16:52:10 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,29 @@ require 'faker'
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "Cleaning date base"
-Spaceship.destroy_all
 
+Spaceship.destroy_all
+User.destroy_all
+
+puts "Creating Users..."
+20.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+  user.save
+  puts "User save"
+  puts "Creating spaceship"
+  5.times do
+    spaceship = Spaceship.new(
+      user_id: user.id,
+      name: Faker::Movies::StarWars.vehicle,
+      description: Faker::Lorem.sentence,
+      max_people: Faker::Number.between(from: 1, to: 10),
+      price: Faker::Commerce.price
+    )
+    spaceship.save
+  end
+end
+
+puts "Finished"
